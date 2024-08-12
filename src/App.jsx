aaff717d2ed67e02b1dff2e253b0f536508e2b7d
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-12 14:22:50 +0300

    Set up routing and state management in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 // src/App.jsx
 
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import MailboxForm from './components/MailboxForm';
+import MailboxList from './components/MailboxList';
+import MailboxDetails from './components/MailboxDetails';
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [mailboxes, setMailboxes] = useState([]);
+
+  const addBox = (box) => {
+    const newBox = {
+      _id: mailboxes.length + 1,
+      ...box,
+    };
+    setMailboxes([...mailboxes, newBox]);
+  };
+
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<main><h1>Post Office</h1></main>} />
+        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
