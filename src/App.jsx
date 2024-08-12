ba32ffd4eaed3b216542aee94753442f3ca910fd
Fadhel-SH <fadhel.sh93bh@gmail.com.com>
2024-08-12 14:31:56 +0300

    Update App.jsx with responsive design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 // src/App.jsx
 
 import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import MailboxForm from './components/MailboxForm';
 import MailboxList from './components/MailboxList';
 import MailboxDetails from './components/MailboxDetails';
@@ -20,13 +19,24 @@ const App = () => {
 
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<main><h1>Post Office</h1></main>} />
-        <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
-        <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
-        <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
-      </Routes>
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/mailboxes">Mailboxes</Link></li>
+            <li><Link to="/new-mailbox">New Mailbox</Link></li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/" element={<h1>Post Office</h1>} />
+          <Route path="/mailboxes" element={<MailboxList mailboxes={mailboxes} />} />
+          <Route path="/new-mailbox" element={<MailboxForm addBox={addBox} />} />
+          <Route path="/mailboxes/:mailboxId" element={<MailboxDetails mailboxes={mailboxes} />} />
+        </Routes>
+      </main>
     </Router>
   );
 };
